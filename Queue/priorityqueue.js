b10d96e65ece7d88451e24b542551aad22ab8eff
Pushkar22022000 <pushkar.kamble@konverge.ai>
2022-02-24 02:05:25 +0530

    master

---
+++ Queue/priorityqueue.js
@@ -0,0 +1,34 @@
+function PriorityQueue() {
+    let items = [];
+    function QueueElement(element, priority) {
+      this.element = element;
+      this.priority = priority;
+    }
+  
+    this.enqueue = function (element, priority) {
+      let queueElement = new QueueElement(element, priority);
+  
+      let added = false;
+      for (let i = 0; i < items.length; i++) {
+        if (queueElement.priority < items[i].priority) {
+          items.splice(i, 0, queueElement);
+          added = true;
+          break;
+        }
+      }
+      if (!added) {
+        items.push(queueElement);
+      }
+    };
+    this.print = function () {
+      for (let i = 0; i < items.length; i++) {
+        console.log(`${items[i].element} - ${items[i].priority}`);
+      }
+    };
+  }
+  
+  let priorityQueue = new PriorityQueue();
+  priorityQueue.enqueue("Pushkar", 1);
+  priorityQueue.enqueue("Piyush", 3);
+  priorityQueue.enqueue("Aniket", 2);
+  priorityQueue.print();
