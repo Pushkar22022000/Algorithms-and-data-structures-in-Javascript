e23bed98b7c858b9c7e4dd272ad857b4907c9172
Pushkar22022000 <pushkar.kamble@konverge.ai>
2022-02-25 02:42:59 +0530

    master

--- Graph/graph.js
+++ Graph/graph.js
@@ -1,41 +1,54 @@
-//  this is breadth first search
 function bfs(graph, root) {
-    let nodesLen = {};
-  
+   
+    let nodesLen = {}
+
+    // start every element with infinity
     for (let i = 0; i < graph.length; i++) {
-      nodesLen[i] = Infinity;
+      nodesLen[i] = Infinity
     }
+    
+    nodesLen[root] = 0 
+    
+    // this keeps the track of nodes
+    let queue = [root] 
+   
+    let current
   
-    nodesLen[root] = 0;
-    let queue = [root];
-    let current;
-  
+    // check if empty if not remove a node. as queue first in first out we use shift
     while (queue.length != 0) {
-      current = queue.shift();
-    }
-  
-    let curConnected = graph[current];
-    let neighborIdx = [];
-    let idx = curConnected.indexOf(1);
-    while (idx != -1) {
-      neighborIdx.push(idx);
-      idx = curConnected.indexOf(1, idx + 1);
-  
+      current = queue.shift()
+      
+     
+      let curConnected = graph[current]
+      
+      let neighborIdx = []
+    
+      let idx = curConnected.indexOf(1)
+      while (idx != -1) {
+        
+        neighborIdx.push(idx)
+        
+        idx = curConnected.indexOf(1, idx + 1)
+      }
+      
+
+      
       for (let j = 0; j < neighborIdx.length; j++) {
-        if (nodesLen[neighborIdx[j]] === Infinity) {
-          nodesLen[neighborIdx[j]] = nodesLen[current] + 1;
-          queue.push(neighborIdx[j]);
+        if (nodesLen[neighborIdx[j]] == Infinity) {
+          nodesLen[neighborIdx[j]] = nodesLen[current] + 1
+          queue.push(neighborIdx[j])
         }
       }
     }
-    return nodesLen;
+    return nodesLen
   }
   
-  let BFSGraph = [
+  let exBFSGraph = [
     [0, 1, 1, 1, 0],
     [0, 0, 1, 0, 0],
     [1, 1, 0, 0, 0],
     [0, 0, 0, 1, 0],
-    [0, 1, 0, 0, 0],
-  ];
-  console.log(bfs(BFSGraph, 1));
+    [0, 1, 0, 0, 0]
+  ]
+  
+  console.log(bfs(exBFSGraph, 1)
