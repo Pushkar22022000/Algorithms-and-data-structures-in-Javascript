80f1e6d05eb0a09242487987e69d41039d112169
Pushkar Kamble <99797632+Pushkar22022000@users.noreply.github.com>
2022-02-25 15:57:58 +0530

    Update graph.js

--- Graph/graph.js
+++ Graph/graph.js
@@ -1,3 +1,4 @@
+/* Graphs: Breadth-first search */
 function bfs(graph, root) {
    
     let nodesLen = {}
@@ -12,31 +13,31 @@ function bfs(graph, root) {
     // this keeps the track of nodes
     let queue = [root] 
    
-    let current
+    let current    //keep track of the current node
   
-    // check if empty if not remove a node. as queue first in first out we use shift
+    // this is to keep traversing nodes until there are no more nodes in the queue
     while (queue.length != 0) {
       current = queue.shift()
       
      
       let curConnected = graph[current]
       
-      let neighborIdx = []
+      let neighborIdx = []  // shows what nodes are connected to the root node
     
-      let idx = curConnected.indexOf(1)
+      let idx = curConnected.indexOf(1) // shows that the node is connected to another node at that index
       while (idx != -1) {
         
-        neighborIdx.push(idx)
+        neighborIdx.push(idx) // looks for next connected node
         
-        idx = curConnected.indexOf(1, idx + 1)
+        idx = curConnected.indexOf(1, idx + 1)   // searches for the next connected node we look for the next connected one 
       }
       
 
-      
+        // value of lin array at the index of neighbor from the neighbor index array equals infinity, no distance set
       for (let j = 0; j < neighborIdx.length; j++) {
         if (nodesLen[neighborIdx[j]] == Infinity) {
-          nodesLen[neighborIdx[j]] = nodesLen[current] + 1
-          queue.push(neighborIdx[j])
+          nodesLen[neighborIdx[j]] = nodesLen[current] + 1  // distance setted
+          queue.push(neighborIdx[j])       //  loop throught these connecting nodes and get the distance
         }
       }
     }
